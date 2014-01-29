7c84d020dcbbcf9506ee491b6180d5e686353235
Apprentice <apprentice@dbc-chicago-6.local>
2014-01-29 14:21:53 -0600

    refactored if/else, added authentication in user model

--- app/controllers/index.rb
+++ app/controllers/index.rb
@@ -7,23 +7,17 @@ get '/' do
 end
 
 post '/' do
-  @user = User.find_by(email: params[:email])
-  if @user == nil
-    "We could not locate a user based upon the email you provided."
-  elsif @user.password == params[:password]
-    session[:id] = @user.id
-    erb :secret_page
-  else
-    "The password you provided is incorrect. Please try again or create an account."
-  end
+  @user = User.authenticate(params[:email], params[:password])
+  redirect to('/') if @user == nil
+
+  session[:id] = @user.id
+  redirect to('/secret')
 end
 
 get '/secret' do
-  if session[:id] == nil
-    redirect to('/')
-  else
-    erb :secret_page
-  end
+  redirect to('/') if session[:id] == nil
+
+  erb :secret_page
 end
 
 get '/logout' do
@@ -36,7 +30,7 @@ get '/sign_up' do
 end
 
 post '/sign_up' do
-  @u = User.create(name: params[:name], email: params[:email], password: params[:password])
+  @u = User.create!(name: params[:name], email: params[:email], password: params[:password])
   session[:id] = @u.id
   erb :secret_page
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,12 @@
 class User < ActiveRecord::Base
-  # Remember to create a migration!
+  validates :email, uniqueness: true
+
+  def self.authenticate(email, password)
+    user = User.find_by(email: email, password: password)
+    if user == nil
+      nil
+    else
+      user
+    end
+  end
 end
